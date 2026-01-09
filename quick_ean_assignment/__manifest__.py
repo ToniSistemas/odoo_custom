ebda414dc5ed4c9044ca9c869c374130a4c14ad6
Toni Iglesias <tiglesias@sistemasdixitais.com>
2026-01-09 20:07:03 +0100

    Bump version to 18.0.1.0.1 to force module update

--- quick_ean_assignment/__manifest__.py
+++ quick_ean_assignment/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Quick EAN Assignment',
-    'version': '18.0.1.0.0',
+    'version': '18.0.1.0.1',
     'category': 'Inventory',
     'summary': 'Assign EAN codes quickly from purchase receipts and mass assignment view',
     'description': """
