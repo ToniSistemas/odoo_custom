c701cc24306c4d95635ec28ef1dfcb73649536de
Toni Iglesias <tiglesias@sistemasdixitais.com>
2026-01-12 10:55:59 +0100

    Fix product_tmpl_id field error in purchase order view - v18.0.1.2.1

--- quick_ean_assignment/__manifest__.py
+++ quick_ean_assignment/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Quick EAN Assignment',
-    'version': '18.0.1.2.0',
+    'version': '18.0.1.2.1',
     'category': 'Inventory',
     'summary': 'Assign EAN codes quickly from purchase receipts and mass assignment view',
     'description': """
