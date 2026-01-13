0e19741fe4f0899dd01f37dc61bd2f9610ddcb3f
Toni Iglesias <tiglesias@sistemasdixitais.com>
2026-01-13 15:33:28 +0100

    Add product label 4x11 format module - v18.0.1.0.0

---
+++ product_label_4x11/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Product Labels 4x11',
+    'version': '18.0.1.0.0',
+    'category': 'Inventory',
+    'summary': 'Add 4x11 product label format',
+    'description': """
+        Product Labels 4x11
+        ===================
+        Adds a new product label format with 4 columns and 11 rows.
+    """,
+    'author': 'Your Company',
+    'website': 'https://www.yourcompany.com',
+    'depends': ['product', 'stock'],
+    'data': [
+        'data/paperformat_data.xml',
+        'report/product_label_report.xml',
+        'views/product_label_template.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
