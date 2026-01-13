b77693c4161b489058a04bdea5ef29c3ed596942
Toni Iglesias <tiglesias@sistemasdixitais.com>
2026-01-13 18:53:59 +0100

    Fix template ID to use 4x12 template - v18.0.1.0.1

--- product_label_apli/__manifest__.py
+++ product_label_apli/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Product Labels APLI 4x11',
-    'version': '18.0.1.0.0',
+    'version': '18.0.1.0.1',
     'category': 'Inventory',
     'summary': 'APLI MF-O1718 label format (4x11)',
     'depends': ['product'],
