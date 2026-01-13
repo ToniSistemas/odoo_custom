ad5fe8da9eb38a3a8954711e0d676a93a0a0b1a1
Toni Iglesias <tiglesias@sistemasdixitais.com>
2026-01-13 19:17:37 +0100

    Add APLI paper format with correct margins - v18.0.1.0.3

--- product_label_apli/__manifest__.py
+++ product_label_apli/__manifest__.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Product Labels APLI 4x11',
-    'version': '18.0.1.0.2',
+    'version': '18.0.1.0.3',
     'category': 'Inventory',
     'summary': 'APLI MF-O1718 label format (4x11)',
     'depends': ['product'],
     'data': [
+        'data/paperformat_data.xml',
         'report/product_label_report.xml',
         'views/product_label_template.xml',
     ],
