deedd00dae525bb7289754b2b06489f0f6c88ada
Toni Iglesias <tiglesias@sistemasdixitais.com>
2026-01-20 13:19:59 +0100

    Dejar _prepare_report_data igual que el estándar, solo cambiando xml_id para 4x11

--- product_label_apli/models/product_label_layout.py
+++ product_label_apli/models/product_label_layout.py
@@ -16,40 +16,4 @@ class ProductLabelLayout(models.TransientModel):
         xml_id, data = super()._prepare_report_data()
         if self.print_format == '4x11_apli':
             xml_id = 'product_label_apli.action_report_product_label_4x11_custom'
-            # Forzar que 'products' siempre tenga contenido
-            Product = self.env['product.product']
-            products = data.get('products')
-                # Refuerzo final: forzar siempre recordset
-                def to_recordset(val):
-                    if hasattr(val, 'ids'):
-                        return val
-                    if isinstance(val, (list, tuple)):
-                        try:
-                            return Product.browse([int(x) for x in val])
-                        except Exception:
-                            return Product.browse([])
-                    if isinstance(val, int):
-                        return Product.browse([val])
-                    if isinstance(val, str):
-                        try:
-                            return Product.browse([int(val)])
-                        except Exception:
-                            return Product.browse([])
-                    return Product.browse([])
-
-                # Intentar con products de data
-                products = to_recordset(products)
-                # Si sigue vacío, buscar en active_ids
-                if not products or not products.ids:
-                    active_ids = self.env.context.get('active_ids')
-                    products = to_recordset(active_ids)
-                # Si sigue vacío, buscar en self.product_ids/product_id
-                if (not products or not products.ids):
-                    if hasattr(self, 'product_ids') and self.product_ids:
-                        products = to_recordset(self.product_ids)
-                    elif hasattr(self, 'product_id') and self.product_id:
-                        products = to_recordset(self.product_id)
-                # Refuerzo final: nunca string
-                products = to_recordset(products)
-                data['products'] = products
         return xml_id, data
