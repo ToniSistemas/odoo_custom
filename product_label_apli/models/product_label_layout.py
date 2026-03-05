ad7d6a61b89071a274eb42a79b0be3b99f8fe25f
Toni Iglesias <tiglesias@sistemasdixitais.com>
2026-03-05 20:30:18 +0100

    Fix product_label_apli - simplify _prepare_report_data to avoid KeyError

---
+++ product_label_apli/models/product_label_layout.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+
+class ProductLabelLayout(models.TransientModel):
+    _inherit = 'product.label.layout'
+
+    print_format = fields.Selection(
+        selection_add=[('4x11_apli', 'Etiquetas 4x11 APLI')],
+        ondelete={'4x11_apli': 'set default'}
+    )
+
+    def _prepare_report_data(self):
+        """Prepare data for report generation"""
+        xml_id, data = super()._prepare_report_data()
+        if self.print_format == '4x11_apli':
+            xml_id = 'product_label_apli.action_report_product_label_4x11_custom'
+        return xml_id, data
