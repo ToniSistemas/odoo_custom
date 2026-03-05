38cbbc462de2825741bd03206b18f7355819d59a
Toni Iglesias <tiglesias@sistemasdixitais.com>
2026-03-05 20:22:23 +0100

    Add ocultar_empaquetado_albaranes module - allows hiding Empaquetado column in delivery orders

---
+++ ocultar_empaquetado_albaranes/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Ocultar Empaquetado en Albaranes',
+    'version': '17.0.1.0.0',
+    'summary': 'Permite ocultar la columna Empaquetado en las líneas de albaranes',
+    'category': 'Warehouse',
+    'author': 'Toni',
+    'website': '',
+    'license': 'LGPL-3',
+    'depends': ['stock'],
+    'data': [
+        'views/stock_picking_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
