4b9ce17f45b36005bc7cdad030f021e5c8aa7d0f
Toni Iglesias <tiglesias@sistemasdixitais.com>
2026-04-09 10:04:45 +0200

    Bump version to 1.3.2

--- vinedo_field_service/__manifest__.py
+++ vinedo_field_service/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Viñedo - Field Service',
-    'version': '1.3.1',
+    'version': '1.3.2',
     'summary': 'Gestión completa de viñedos: fincas, variedades, trabajos y añadas con mapas',
     'description': """
         Gestión de Viñedos
