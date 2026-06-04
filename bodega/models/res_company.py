f4e64145101c1a8b5db6904ec3cc830e4d750ca2
Toni Iglesias <tiglesias@sistemasdixitais.com>
2026-06-04 13:28:12 +0200

    feat(bodega): add NIDPB field to company form

--- bodega/models/res_company.py
+++ bodega/models/res_company.py
@@ -4,6 +4,11 @@ from odoo import models, fields
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
+    bodega_nidpb = fields.Char(
+        string='NIDPB (Número de Identificación de la Bodega de Producción)',
+        help='Número de Identificación de la Bodega de Producción asignado '
+             'por el organismo competente.',
+    )
     bodega_ria = fields.Char(
         string='RIA (Registro de Instalaciones de Actividad)',
         help='Número de inscripción en el Registro de Instalaciones de Actividad '
